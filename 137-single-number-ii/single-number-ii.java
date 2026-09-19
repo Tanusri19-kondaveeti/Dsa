b0accf080441b94a5ec3a4e23e8d673744a8f716
tanusri19 <tanusrikondaveeti19@gmail.com>
2026-09-19 16:55:51 +0530

    Time: 6 ms (43.73%) | Memory: 46.3 MB (24.73%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,35 +1,48 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int prev = -1;
-        int next = -1;
-        if(nums.length == 1)
-        {
-            return nums[0];
-        }
+        // Arrays.sort(nums);
+        // int prev = -1;
+        // int next = -1;
+        // if(nums.length == 1)
+        // {
+        //     return nums[0];
+        // }
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(i ==0 && nums[i] != nums[i+1])
+        //     {
+        //         return nums[i];
+        //     }
+        //     else if(i ==0 && nums[i] == nums[i+1])
+        //     {
+        //         continue;
+        //     }
+        //     if(i==nums.length-1 && nums[i] != nums[i-1])
+        //     {
+        //         return nums[i];
+        //     }
+        //     else if(i == nums.length-1 && nums[i] == nums[i-1])
+        //     {
+        //         continue;
+        //     }
+        //     prev = nums[i-1];
+        //     next = nums[i+1];
+        //     if(nums[i] != prev && nums[i] != next)
+        //     {
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
-            if(i ==0 && nums[i] != nums[i+1])
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int n : map.keySet())
+        {
+            if(map.get(n) == 1)
             {
-                return nums[i];
-            }
-            else if(i ==0 && nums[i] == nums[i+1])
-            {
-                continue;
-            }
-            if(i==nums.length-1 && nums[i] != nums[i-1])
-            {
-                return nums[i];
-            }
-            else if(i == nums.length-1 && nums[i] == nums[i-1])
-            {
-                continue;
-            }
-            prev = nums[i-1];
-            next = nums[i+1];
-            if(nums[i] != prev && nums[i] != next)
-            {
-                return nums[i];
+                return n;
             }
         }
         return -1;
